e1b2130c457a4ee95f16ecb807f0986a18fc14ff
Zaira Rasool <44139464+zraso@users.noreply.github.com>
2023-07-28 15:27:43 +0000

    Create register_page.dart

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,217 @@
+import 'package:fabric_app/components/my_button.dart';
+import 'package:fabric_app/components/square_title.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+
+import '../components/text_field.dart';
+
+class RegisterPage extends StatefulWidget {
+  final Function()? onTap;
+  const RegisterPage({super.key, required this.onTap});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  // Text editing controllers
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+
+  // sign user up method
+  void signUserUp() async {
+    // show loading circle
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },);
+
+    // try creating the user
+    try {
+      // check if password is confirmed
+      if (passwordController.text == confirmPasswordController.text){
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: emailController.text,
+          password: passwordController.text,
+        );
+      } else {
+        // show error message, passwords don't match
+        showErrorMessage("Password don't match!");
+      }
+
+      // pop the loading circle
+      Navigator.pop(context);
+    } on FirebaseAuthException catch (e) {
+      // pop the loading circle
+      Navigator.pop(context);
+      // show error message
+      showErrorMessage(e.code);
+
+
+
+    }
+  }
+
+  // error message to user
+  void showErrorMessage(String message){
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            backgroundColor: Colors.grey,
+            title: Center(
+              child: Text(
+                message,
+                style: const TextStyle(color: Colors.grey),
+              ),
+            ),
+          );
+        }
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 50),
+                // logo
+                const Icon(
+                  Icons.lock,
+                  size: 100,
+                  color: Colors.black,
+                ),
+                const SizedBox(height: 25.0),
+
+                // lets get started by creating an account
+                const Text(
+                  "let's get started",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                  ),
+                ),
+                const SizedBox(height: 25),
+
+                // email text field
+                MyTextField(
+                  controller: emailController,
+                  hintText: 'Email',
+                  obscureText: false,
+                ),
+
+                // password text field
+                MyTextField(
+                  controller: passwordController,
+                  hintText: 'password',
+                  obscureText: true,
+                ),
+                const SizedBox(height: 25),
+                // confirm password text field
+                MyTextField(
+                  controller: confirmPasswordController,
+                  hintText: 'confirm password',
+                  obscureText: true,
+                ),
+                const SizedBox(height: 25),
+
+
+                const SizedBox(height: 25),
+
+                // sign in button
+                MyButton(
+                  text: "Sign Up",
+                  onTap: signUserUp,
+                ),
+                const SizedBox(height: 25.0),
+
+
+                // or continue with
+                const Padding(
+                  padding:EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Text(
+                          'Or continue with',
+                          style: TextStyle(color: Colors.grey)
+                          ,
+
+                        ),
+                      ),
+
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Colors.grey,
+                        ),
+                      ),
+
+                    ],
+                  ),
+                ),
+
+                // google + facebook + apple
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // google button
+                    SquareTile(imagePath: 'lib/assets/Google__G__Logo.svg.png'),
+                    SizedBox(width: 7.0),
+
+                    // apple button
+                    SquareTile(imagePath: 'lib/assets/Apple_logo_black.svg.png'),
+                    SizedBox(width: 7.0),
+
+                    // facebook button
+                    SquareTile(imagePath: 'lib/assets/facebook_logo.png'),
+                  ],
+                ),
+                const SizedBox(height: 50.0),
+
+                // don't have an account? create
+                Row(
+                  children: [
+                    const Text(
+                      "Already have an account?",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    const SizedBox(width: 4.0),
+                    GestureDetector(
+                      onTap: widget.onTap,
+                      child: const Text(
+                        "Login Now",
+                        style: TextStyle(
+                          color: Colors.lightBlue,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
