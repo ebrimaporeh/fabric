51b879216a1ae42386f891d04726df296fe6ef91
Zaira Rasool <44139464+zraso@users.noreply.github.com>
2023-07-28 15:13:47 +0000

    Create login_or_register_page.dart

---
+++ lib/pages/login_or_register_page.dart
@@ -0,0 +1,36 @@
+import 'package:fabric_app/pages/login_page.dart';
+import 'package:fabric_app/pages/register_page.dart';
+import 'package:flutter/material.dart';
+
+class LoginOrRegisterPage extends StatefulWidget {
+  const LoginOrRegisterPage({super.key});
+
+  @override
+  State<LoginOrRegisterPage> createState() => _LoginOrRegisterPageState();
+}
+
+class _LoginOrRegisterPageState extends State<LoginOrRegisterPage> {
+  // initially show login page
+  bool showLoginPage = true;
+
+  // toggle between login and register page
+  void togglePage() {
+    setState(() {
+      showLoginPage = !showLoginPage;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    if (showLoginPage) {
+      return LoginPage(
+        onTap: togglePage,
+      );
+    } else {
+      return RegisterPage(
+          onTap: togglePage,
+
+      );
+    }
+
+  }
+}
