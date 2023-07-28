17eb80b5c6757e84c1b0b54637a070872ed9a499
Zaira Rasool <44139464+zraso@users.noreply.github.com>
2023-07-28 15:12:39 +0000

    Create auth.dart

---
+++ lib/pages/auth.dart
@@ -0,0 +1,28 @@
+import 'package:fabric_app/pages/login_or_register_page.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'Home_page.dart';
+import 'package:fabric_app/pages/login_page.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          // User is logged on
+          if (snapshot.hasData) {
+            return HomePage();
+          }
+
+          // user is NOT logged on
+          else {
+            return const LoginOrRegisterPage();
+          }
+        },
+      ),
+    );
+  }
+}
